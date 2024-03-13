8ba2b42064138718beb26e43db807770cd09f1b9
salit-av <salit.avramovich@gmail.com>
2024-03-13 15:17:15 +0200

    Strategy works?

---
+++ FacebookWinFormsApp/GuessStrategy.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Globalization;
+using FacebookWrapper.ObjectModel;
+
+namespace BasicFacebookFeatures
+{
+    interface IGuessStrategy
+    {
+        bool IsGuessCorrect(object itemToGuess, object guess);
+    }
+
+    class GuessPostYearStrategy : IGuessStrategy
+    {
+        public bool IsGuessCorrect(object itemToGuess, object guess)
+        {
+            Post postToGuess = itemToGuess as Post;
+
+            // Parse the guess string into an integer
+            if (int.TryParse(guess as string, out int guessedYear))
+            {
+                if (postToGuess != null && postToGuess.CreatedTime.HasValue)
+                {
+                    return postToGuess.CreatedTime.Value.Year == guessedYear;
+                }
+            }
+
+            return false;
+        }
+    }
+
+    class GuessBirthdayMonthStrategy : IGuessStrategy
+    {
+        public bool IsGuessCorrect(object itemToGuess, object guess)
+        {
+            User friendToGuess = itemToGuess as User;
+            int guessedMonth = (int)guess;
+
+            if (friendToGuess != null && !string.IsNullOrWhiteSpace(friendToGuess.Birthday))
+            {
+                DateTime birthdayDate = DateTime.ParseExact(friendToGuess.Birthday, "MM/dd/yyyy", CultureInfo.InvariantCulture);
+                
+                return birthdayDate.Month == guessedMonth;
+            }
+
+            return false;
+        }
+    }
+}
